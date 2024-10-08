f21cc086119000bff2dba422aad6cff9066fda9a
JWS <1041139466@qq.com>
2024-10-08 10:56:51 +0800

    删除无关文件

--- CXL_malloc/CXL_apidef.cpp
+++
@@ -1,8 +0,0 @@
-#include<unordered_map>
-#include"CXL_api.h"
-
-struct CXL_area* find_block(
-    const std::unordered_map<int, memtable_entry*>& memtable, int size,
-    bool isshared, int host_id) {
-
-};
